ed209af4fd319f13e404a8779dd140a6d4609d99
koohzin <joao.koguishi@gmail.com>
2021-08-30 19:08:48 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function somar(){
+    var num1 = document.querySelector(".num1").value
+    var num2 = document.querySelector(".num2").value
+
+    var resultado = parseInt(num1) + parseInt(num2)
+    document.querySelector(".resultado").innerHTML = resultado
+}
+
+function subtrair(){
+    var num1 = document.querySelector(".num1").value
+    var num2 = document.querySelector(".num2").value
+
+    var resultado = parseInt(num1) - parseInt(num2)
+    document.querySelector(".resultado").innerHTML = resultado
+}
+
+function dividir(){
+    var num1 = document.querySelector(".num1").value
+    var num2 = document.querySelector(".num2").value
+
+    var resultado = parseInt(num1) / parseInt(num2)
+    document.querySelector(".resultado").innerHTML = resultado
+}
+
+function multiplicar(){
+    var num1 = document.querySelector(".num1").value
+    var num2 = document.querySelector(".num2").value
+
+    var resultado = parseInt(num1) * parseInt(num2)
+    document.querySelector(".resultado").innerHTML = resultado
+}
